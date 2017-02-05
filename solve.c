db11cf9b1edc99163b5afe7f2e624924ac908705
Artem Musel <amusel@e1r9p3.unit.ua>
2017-02-05 18:27:16 +0200

    all

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: amusel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 14:03:55 by amusel            #+#    #+#             */
-/*   Updated: 2017/02/05 18:24:39 by amusel           ###   ########.fr       */
+/*   Updated: 2017/02/05 18:27:06 by amusel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,7 @@ char	*solve(va_list ap, struct s_lis *temp, char *ret)
 {
 	char	*buf;
 	wchar_t *tmp;
-	char	*t;
+	char *t;
 
 	tmp = (wchar_t *)malloc(sizeof(tmp) * 100);
 	if (temp->flag == 'S' || (temp->flag == 's' && temp->type[0] == 'l') || \
@@ -135,9 +135,10 @@ char	*solve(va_list ap, struct s_lis *temp, char *ret)
 	else
 		buf = solvechar(ap, temp);
 	t = ret;
-	ret = (char *)malloc(sizeof(ret) * (ft_strlen(ret) + ft_strlen(buf) + 10));
-	ret = t;
-	ret = ft_strjoin(ret, buf);
+	ret = NULL;
+	free(ret);
+	ret = (char *)malloc(sizeof(ret) * (ft_strlen(t) + ft_strlen(buf) + 10));
+	ret = ft_strjoin(t, buf);
 	temp->j = (int)ft_strlen(ret);
 	return (ret);
 }

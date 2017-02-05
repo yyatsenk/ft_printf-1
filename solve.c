e88cbc2a5e234ba279e249afb10efdcf71238a95
Artem Musel <amusel@e1r9p3.unit.ua>
2017-02-05 18:24:43 +0200

    all

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: amusel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 14:03:55 by amusel            #+#    #+#             */
-/*   Updated: 2017/01/29 14:04:05 by amusel           ###   ########.fr       */
+/*   Updated: 2017/02/05 18:24:39 by amusel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,6 +117,7 @@ char	*solve(va_list ap, struct s_lis *temp, char *ret)
 {
 	char	*buf;
 	wchar_t *tmp;
+	char	*t;
 
 	tmp = (wchar_t *)malloc(sizeof(tmp) * 100);
 	if (temp->flag == 'S' || (temp->flag == 's' && temp->type[0] == 'l') || \
@@ -133,7 +134,9 @@ char	*solve(va_list ap, struct s_lis *temp, char *ret)
 		buf = solveint(ap, temp, "", "");
 	else
 		buf = solvechar(ap, temp);
+	t = ret;
 	ret = (char *)malloc(sizeof(ret) * (ft_strlen(ret) + ft_strlen(buf) + 10));
+	ret = t;
 	ret = ft_strjoin(ret, buf);
 	temp->j = (int)ft_strlen(ret);
 	return (ret);

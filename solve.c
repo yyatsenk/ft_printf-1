ea9b2623b67458b077abb6e984ee5ccbc14a36e4
Artem Musel <amusel@e1r9p3.unit.ua>
2017-02-05 17:52:36 +0200

    all

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: amusel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 14:03:55 by amusel            #+#    #+#             */
-/*   Updated: 2017/01/29 14:04:05 by amusel           ###   ########.fr       */
+/*   Updated: 2017/02/05 17:52:33 by amusel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,10 +129,11 @@ char	*solve(va_list ap, struct s_lis *temp, char *ret)
 		temp->j = 0;
 		return (ret);
 	}
-	else if (ft_memchr("dDiuUoOxX", temp->flag, 9) > 0)
-		buf = solveint(ap, temp, "", "");
-	else
-		buf = solvechar(ap, temp);
+	buf = '\0';
+//	else if (ft_memchr("dDiuUoOxX", temp->flag, 9) > 0)
+//		buf = solveint(ap, temp, "", "");
+//	else
+//		buf = solvechar(ap, temp);
 	ret = ft_strjoin(ret, buf);
 	temp->j = (int)ft_strlen(ret);
 	return (ret);

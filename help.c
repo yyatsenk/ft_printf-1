c39f4bf0ced0c50d230ea97767659f80b7c04059
Artem Musel <amusel@e1r8p4.unit.ua>
2017-02-06 12:47:53 +0200

    all

--- help.c
+++ help.c
@@ -20,7 +20,7 @@ void	clcsls(va_list ap, struct s_lis *temp, wchar_t *tmp)
 	if (temp->flag == 'S' || temp->flag == 's')
 		temp->lsc = (wchar_t *)va_arg(ap, int *);
 	if (temp->flag == 'C' || temp->flag == 'c')
-		tmp[0] = (wchar_t)va_arg(ap, int);
+		tmp[0] = '�';
 	if (temp->flag != 'C')
 		while (++i < temp->prec)
 			tmp[i] = temp->lsc[i];
